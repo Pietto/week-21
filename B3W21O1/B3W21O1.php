169b10640a66f73e56eaab63d4769386c40215c6
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2019-03-25 09:28:37 +0100

    .

--- B3W21O1/B3W21O1.php
+++ B3W21O1/B3W21O1.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 	<title>B3W21O1</title>
-	 <link rel="stylesheet" type="text/css" href="B3W21O1.css">
+	<link rel="stylesheet" type="text/css" href="B3W21O1.css">
 </head>
 <body>
 	<?php
-		date_default_timezone_set('Europe/Amsterdam');
+		date_default_timezone_set('America/Chicago');
 		$time = date("H:i");
 
 		if($time>=6 && $time<=12){
@@ -14,13 +14,13 @@
 		    echo "<div class='container' id='morning'><h1>good morning!<br> the time is $time</h1></div>";
 		}elseif($time>=18 && $time<=24){
 		    $image = "evening";
-		    echo "<div class='container' id='evening'><h1>good morning!<br> the time is $time</h1></div>";
+		    echo "<div class='container' id='evening'><h1>good evening!<br> the time is $time</h1></div>";
 		}elseif($time>=12 && $time<=18){
 		    $image = "afternoon";
-		    echo "<div class='container' id='afternoon'><h1>good morning!<br> the time is $time</h1></div>";
+		    echo "<div class='container' id='afternoon'><h1>good afternoon!<br> the time is $time</h1></div>";
 		}elseif($time>=0 && $time<=6){
 		    $image = "night";
-		    echo "<div class='container' id='night'><h1>good morning!<br> the time is $time</h1></div>";
+		    echo "<div class='container' id='night'><h1>good night!<br> the time is $time</h1></div>";
 		}
 	?>
 </body>
---
+++ Website.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Website - Labs</title>
+	<link rel="stylesheet" type="text/css" href="Website.css">
+</head>
+<body>
+	<?php
+		include 'include/header.html';
+		include 'include/menu.html';
+
+
+
+
+
+		include 'include/footer.html';
+	?>
+</body>
+</html>
